5940799fe020147f21023719905bd644650d7ac8
dstovell <ninja@Daniels-iMac.local>
2017-10-28 03:58:49 -0700

    Serializer and Net messages!!! Fun night tomorrow night

---
+++ TacticalBoard/Net/NetMessage.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TacticalBoard
+{
+	public class Handshake : NetMessage
+	{
+		public uint secret;
+		public uint playerId;
+		public uint gameId;
+
+		public Handshake() : base(Type.Handshake)
+		{
+		}
+
+		public override void OnSerialize(Serializer s)
+		{
+			base.OnSerialize(s);
+			this.secret = s.Serialize(secret);
+			this.playerId = s.Serialize(playerId);
+			this.gameId = s.Serialize(gameId);
+		}
+	}
+
+	public class NetMessage : Serializable
+	{
+		public enum Type
+		{
+			Handshake
+		}
+		public Type MessageType;
+
+		public NetMessage(Type type)
+		{
+			this.MessageType = type;
+		}
+
+		public override void OnSerialize(Serializer s)
+		{
+			this.MessageType = (Type)s.Serialize((int)this.MessageType);
+		}
+	}
+}
+
---
+++ TacticalBoard/Net/Serializer.cs
@@ -0,0 +1,109 @@
+﻿using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TacticalBoard
+{
+	public abstract class Serializable
+	{
+		public abstract void OnSerialize(Serializer s);
+	}
+	
+	public abstract class Serializer
+	{
+		protected uint index;
+		protected byte [] data;
+		public Serializer(byte [] _data)
+		{
+			this.data = _data;
+			this.index = 0;
+		}
+
+		public abstract int Serialize(int n);
+		public abstract uint Serialize(uint n);
+		public abstract ushort Serialize(ushort n);
+		//public abstract void Serialize(float n);
+	}
+
+	public class WriteSerializer : Serializer
+	{
+		public WriteSerializer(byte [] _data) : base(_data)
+		{
+		}
+
+		protected void WriteBytes(byte [] newData)
+		{
+			for (int i=0; i<newData.Length; i++)
+			{
+				if (this.index == this.data.Length)
+				{
+					return;
+				}
+
+				this.data[this.index] = newData[i];
+				this.index++;
+			}
+		}
+
+		public override int Serialize(int n)
+		{
+			this.WriteBytes( System.BitConverter.GetBytes(n) );
+			return n;
+		}
+
+		public override uint Serialize(uint n)
+		{
+			this.WriteBytes( System.BitConverter.GetBytes(n) );
+			return n;
+		}
+
+		public override ushort Serialize(ushort n)
+		{
+			this.WriteBytes( System.BitConverter.GetBytes(n) );
+			return n;
+		}
+	}
+
+	public class ReadSerializer : Serializer
+	{
+		public ReadSerializer(byte [] _data) : base(_data)
+		{
+		}
+
+		protected void ReadBytes(byte [] newData)
+		{
+			for (int i=0; i<newData.Length; i++)
+			{
+				if (this.index == this.data.Length)
+				{
+					return;
+				}
+
+				newData[i] = this.data[this.index];
+				this.index++;
+			}
+		}
+
+		public override int Serialize(int n)
+		{
+			byte [] newData = new byte[4];
+			this.ReadBytes(newData);
+			return System.BitConverter.ToInt32(newData, 0);
+		}
+
+		public override uint Serialize(uint n)
+		{
+			byte [] newData = new byte[4];
+			this.ReadBytes(newData);
+			return System.BitConverter.ToUInt32(newData, 0);
+		}
+
+		public override ushort Serialize(ushort n)
+		{
+			byte [] newData = new byte[2];
+			this.ReadBytes(newData);
+			return System.BitConverter.ToUInt16(newData, 0);
+		}
+	}
+}
+
